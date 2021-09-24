4ae38c7b97e7cab378dd12c3283c1e59b8b25568
Orius <32719997+ItsOrius@users.noreply.github.com>
2021-09-24 10:36:29 -0400

    Added "Redemption" and "Horrible Person" endings

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ function runGameLoop() {
         let num1 = prompt("Please enter your first number!");
         if (!parseFloat(num1)) {
 
-            if (num1.toLowerCase().includes("fuck you")) {
-                alert("Well fuck you too, buddy!");
-                return endGame("Malding");
+            ai.anger++;
+
+            if (num1 != null) {
+                if (num1.toLowerCase().includes("fuck you")) {
+                    alert("Well fuck you too, buddy!");
+                    return endGame("Malding");
+                }
             }
 
             num1 = prompt("Sorry to break it to you, but I can only handle decimal numbers.\nPlease enter a **VALID** number this time!")
@@ -34,22 +38,39 @@ function runGameLoop() {
         let num2 = prompt("Please enter your second number!");
         if (!parseFloat(num2)) {
 
-            if (num2.toLowerCase().includes("fuck you")) {
-                alert("Well fuck you too, buddy!");
-                return endGame("Malding");
+            ai.anger++;
+
+            if (num2 != null) {
+                if (num2.toLowerCase().includes("fuck you")) {
+                    alert("Well fuck you too, buddy!");
+                    return endGame("Malding");
+                }
             }
 
-            num2 = prompt("Sorry to break it to you, but I can only handle decimal numbers.\nPlease enter a **VALID** number this time!")
-            if (!parseFloat(num2)) {
+            if (ai.anger >= 2) {
+                num2 = prompt("How hard is this to understand?\nENTER. A. VALID. NUMBER.")
+            } else {
+                num2 = prompt("Sorry to break it to you, but I can only handle decimal numbers.\nPlease enter a **VALID** number this time!")
+            }
+
+            if (!parseFloat(num2) && ai.anger < 2) {
                 alert("Alright, alright. That's enough. Goodbye.");
                 return endGame("Math Illiterate")
+            } else if (!parseFloat(num2) && ai.anger >= 2) {
+                alert("What kind of a monster are you?\nYou made me suffer till the very end.\nNever come back.");
+                return endGame("Horrible Person")
             } else num2 = parseFloat(num2);
 
         } else num2 = parseFloat(num2);
 
         // runs if no special endings happened
-        alert(`The result is ${num1+num2}.\nThank you for visiting!`);
-        return endGame("Good");
+        if (ai.anger > 1) {
+            alert(`FINALLY. Your result is ${num1+num2}.\nTry harder next time!`);
+            return endGame("Redemption");
+        } else {
+            alert(`The result is ${num1+num2}.\nThank you for visiting!`);
+            return endGame("Good");
+        }
 
     } else {
 
@@ -59,10 +80,6 @@ function runGameLoop() {
     }
 
 }
-
-
-
-// gets an anger 
 
 
 
